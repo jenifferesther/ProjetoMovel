5fa1787e5386e50c606540a2462f45b625f16e7f
jeniffer Esther <jes21@aluno.ifal.edu.br>
2024-02-26 16:07:22 -0300

    adiciona componete cat

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Cat = () => {
+  return <Text>Hello, I am Cat!</Text>
+
+}
+
+
+export default Cat;
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
